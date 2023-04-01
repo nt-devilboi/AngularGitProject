a96aa6cbca236117920c2b8324e070a8f7a4b6ab
Devilboi99 <turnickii.n@gmail.com>
2023-04-01 15:06:27 +0500

    Закинул идей Главного Интерфейса

--- src/app/shared/interfaces/User.ts
+++ src/app/shared/interfaces/User.ts
@@ -1,7 +1,23 @@
+import {Commit} from "./Commit";
+import {Project} from "./Project";
+import {RepositoryCommit} from "./RepositoryCommit";
+
 export interface User {
   id: number,
   username: string,
   name: string,
   avatar_url: string,
   web_url: string
+}
+
+// как идея. в итоге у этого интерфеса будут все данные
+export interface UserAllData {
+  id: number,
+  username: string,
+  name: string,
+  avatar_url: string,
+  web_url: string
+  commits: Commit[]
+  projects: Project[]
+  RepositoryCommits: RepositoryCommit[]
 }
