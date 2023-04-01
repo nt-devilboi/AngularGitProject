c8b909ee62930922fe5b1dfd4e1ea12949416cff
Devilboi99 <turnickii.n@gmail.com>
2023-04-01 15:05:58 +0500

    добави в конструктор главный сервис

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import {Component} from '@angular/core';
-import {HttpRequestService} from "./http-request.service";
+import {DataUsers} from "./shared/Services/data-Users";
 
 @Component({
   selector: 'app-root',
@@ -10,10 +10,10 @@ export class AppComponent {
   title = 'ProjectArtsofte';
   data = []
 
-  constructor(private gitlab: HttpRequestService) {
+  constructor(private gitlab: DataUsers) {
   }
 
-  click() {
+  Click() {
     this.gitlab.GetData('https://gitlab.com/api/v4/users/927908/projects').subscribe();
 
   }
