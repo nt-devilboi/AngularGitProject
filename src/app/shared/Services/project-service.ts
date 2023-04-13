ac8561def0f5188dd266424335ef0b79e448cbce
caiman <shimyshimya@mail.ru>
2023-04-13 18:45:57 +0500

    replacing routing

--- src/app/shared/Services/project-service.ts
+++ src/app/shared/Services/project-service.ts
@@ -1,6 +1,4 @@
 import {Injectable} from '@angular/core';
-import {HttpClient, HttpClientModule} from "@angular/common/http";
-import {Observable, of} from "rxjs";
 import {HttpRequestService} from "./http-request.service";
 
 @Injectable({
