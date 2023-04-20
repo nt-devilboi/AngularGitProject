8b34d15d78dfeebdfac9ede0272d70ed8959c704
caiman <shimyshimya@mail.ru>
2023-04-20 12:57:36 +0500

    доделал верстку header

--- src/app/pages/main-page/components/header/header.component.ts
+++ src/app/pages/main-page/components/header/header.component.ts
@@ -1,10 +1,14 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss']
 })
-export class HeaderComponent {
+export class HeaderComponent{
+  protected searchById: boolean = true;
 
+  switchSearchMethod() {
+    this.searchById = !this.searchById;
+  }
 }
