ba3915770e4622479bc356a4bea67e4225bb08ce
Devilboi99 <turnickii.n@gmail.com>
2023-04-21 21:13:42 +0500

    убрал лишний код

--- src/app/pages/main-page/components/header/header.component.ts
+++ src/app/pages/main-page/components/header/header.component.ts
@@ -1,6 +1,5 @@
 import {Component} from '@angular/core';
 import {FormControl, FormGroup, Validators} from "@angular/forms";
-import {ValidatorIsIdUserNotName} from "../../ValidatorIsIdUserNotName";
 
 @Component({
   selector: 'app-header',
